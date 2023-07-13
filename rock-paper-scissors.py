97d0995212203f33dff2e67af9e7842090561674
Dagna Schmidt <dagdagi889@gmail.com>
2023-07-13 11:16:41 +0200

    paper rock scissors

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,5 +1,19 @@
-computer_choice = 'scissors'
-user_choice = input("Choose rock, paper or scissors")
+import random
+
+choices = ['paper', 'rock', 'scissors']
+
+computer_choice = random.choice(choices)
+user_choice = input("Choose rock, paper or scissors\n")
+
+print('Computer choice: ' + computer_choice )
 
 if computer_choice == user_choice:
     print('TIE')
+elif computer_choice == 'rock' and user_choice == 'paper':
+    print('WIN')
+elif computer_choice == 'scissors' and user_choice == 'rock':
+    print('WIN')
+elif computer_choice == 'paper' and user_choice == 'scissors':
+    print('WIN')
+else:
+    print('LOSE')
